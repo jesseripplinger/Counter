4fa175b20a4dc5f2c3d540210bec9ffad18543b2
jesseripplinger <jesse.r109@slcstudents.org>
2022-02-09 14:59:42 -0700

    Almost done with the clicker

---
+++ app.js
@@ -0,0 +1,15 @@
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll(".btn")
+
+btns.foreach(function (btn) {
+btn.addEventListener('click', function(e){
+  const styles = e.currentTarget.classList;
+  if(styles.contains('decrease')0{
+     
+     }
+  });    
+});

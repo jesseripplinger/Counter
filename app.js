e2d32d0c79bc0e5666cd3b0891315ce5badbefc5
jesseripplinger <jesse.r109@slcstudents.org>
2022-03-11 14:23:40 -0700

    done with the counter

--- app.js
+++ app.js
@@ -2,14 +2,29 @@
 let count = 0;
 
 // select value and buttons
-const value = document.querySelector('#value')
-const btns = document.querySelectorAll(".btn")
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
 btns.foreach(function (btn) {
 btn.addEventListener('click', function(e){
   const styles = e.currentTarget.classList;
-  if(styles.contains('decrease')0{
-     
+  if(styles.contains("decrease") {
+   count--;  
      }
+     else if(styles.contains("increase")) {
+      count++;
+  } else{
+        count = 0;
+    }
+    if(count > 0) {
+        value.style.color = "green";
+    }
+    if(count < 0){
+      value.style.color = "red"  
+    }
+    if(count === 0){
+        value.style.color = "#222"
+    }
+     value.textContent = count;
   });    
 });
